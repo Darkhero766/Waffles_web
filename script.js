669577369d8bfae91fcb1eadc95659e5ff876272
Faizal <nightowlclub72@gmail.com>
2025-06-25 10:49:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,41 @@
+
 const facts = [
-      "Stars are born in massive clouds of dust and gas.",
-      "Neutron stars can spin 600 times per second!",
-      "The Sun accounts for 99.86% of the Solar System’s mass.",
-      "Space is completely silent.",
-      "One million Earths could fit inside the Sun."
-              ];
+    "Stars are born in massive clouds of dust and gas.",
+    "Neutron stars can spin 600 times per second!",
+    "The Sun accounts for 99.86% of the Solar System's mass.",
+    "Space is completely silent.",
+    "One million Earths could fit inside the Sun."
+];
 
-
-function showFacts() {
-    const spreadFacts = [... facts];
-    const randomFacts = spreadFacts[Math.floor(Math.random() * spreadFacts.length)];
-    const factbox = document.getElementById("fact-box");
+function showFact() {
+    const randomFact = facts[Math.floor(Math.random() * facts.length)];
+    const factBox = document.getElementById("fact-box");
     if (factBox) {
-        factbox.innerText = randomFacts;
-
+        factBox.innerText = randomFact;
     }
 }
 
 function updatePreview() {
-    const name = getElementById("starName").value;
-    const preview = getElementById("livePreview");
+    const name = document.getElementById("starName").value;
+    const preview = document.getElementById("livePreview");
     if (preview) {
         preview.innerHTML = `⭐ Live Preview: <em>${name || '...'}</em>`;
     }
 }
 
-function validateForm(){
+function validateForm() {
     const name = document.getElementById("starName").value.trim();
     const message = document.getElementById("submitMessage");
 
     if (!message) return false;
     
-
-    if (name.length > 3) {
-        message.innerText = "🚫 Star name must be 3 characters";
+    if (name.length < 3) {
+        message.innerText = "🚫 Star name must be at least 3 characters";
         message.style.color = 'red';
         return false;
     }
 
     message.innerText = `🌟 Your star "${name}" has been registered in the galaxy!`;
-    message.style.color = 'lightgreen'
+    message.style.color = 'lightgreen';
     return false;
-
 }
-
